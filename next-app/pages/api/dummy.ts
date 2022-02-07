272f9ac71c319570bd5f0d03fb959482462a9258
marc-town <asamin.itconsultant@gmail.com>
2022-02-07 22:30:57 +0900

    add useswr sample

---
+++ next-app/pages/api/dummy.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+// import { executeQuery } from '@/infrastructure/database/mysql';
+
+export interface DummyData {
+  name: string;
+  // result: string;
+}
+
+export default function dammyApiHandler(req: NextApiRequest, res: NextApiResponse<DummyData>) {
+  try {
+    // const query = 'define sql statemants and ? is replaced values';
+    // const values = ['value1'];
+    // const result = await executeQuery(query, values);
+    // const dummyData: DummyData = {
+    //   name: 'dummy',
+    //   result,
+    // };
+    res.status(200).json({ name: 'called api' });
+  } catch (e) {
+    res.status(500);
+  }
+}
